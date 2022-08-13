baca830db64331ec4cda0517cd48fa253ec0b43e
ok-lab <60685175+ceo21ckim@users.noreply.github.com>
2022-08-13 13:35:35 +0900

    Add files via upload

--- Deep Learning/Natural Language Processing/Gate Recurrent Unit/Gate Recurrent Unit.py
+++ Deep Learning/Natural Language Processing/Gate Recurrent Unit/Gate Recurrent Unit.py
@@ -133,4 +133,36 @@ def train(inputs, labels, model, criterion, optimizer, max_grad_norm=None):
         torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
     optimizer.step()
 
-    return loss 
+    return loss
+
+
+def generate_sentence_from_bos(model, vocab, bos=1):
+    indice = [bos]
+    hidden = torch.zeros((1, 1, model.hidden_size))
+    lm_inputs = torch.tensor(indice).unsqueeze(-1)
+    i2v = {v:k for k, v in vocab.items()}
+
+    cnt = 0
+    eos = vocab['</s>']
+    generated_sequence = [lm_inputs[0].data.item()]
+    while True:
+        if cnt == 30:
+            break 
+        output, hidden = model(lm_inputs, hidden)
+        output = output.sequeeze(1)
+        topv, topi = output.topk(1)
+        lm_inputs = topi 
+
+        if topi.data.item() == eos:
+            tokens = list(map(lambda w: i2v[w], generated_sequence))
+            generated_sentence = ' '.join(tokens)
+            return generated_sentence 
+        
+        generated_sequence.append(topi.data.item())
+        cnt += 1
+
+    print('max iteration reached. therefore finishing forcefully')
+    tokens = list(map(lambda w: i2v[w], generated_sequence))
+
+    generated_sentence = ' '.join(tokens)
+    return generated_sentence 
